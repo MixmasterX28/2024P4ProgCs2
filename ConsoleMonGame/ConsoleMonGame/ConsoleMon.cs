35400b597b9fff22d86289c25b7090f34944f50b
MixmasterX28 <144898918+MixmasterX28@users.noreply.github.com>
2024-05-13 10:51:17 +0200

    3 en 4 af

--- ConsoleMonGame/ConsoleMonGame/ConsoleMon.cs
+++ ConsoleMonGame/ConsoleMonGame/ConsoleMon.cs
@@ -8,8 +8,20 @@ namespace ConsoleMonGame
 {
     internal class ConsoleMon
     {
-       internal int health;
-       internal int energy;
-       internal string name;
+        internal int health;
+        internal int energy;
+        internal string name;
+
+
+        internal void TakeDamage(int damage)
+        {
+            health -= damage;
+        }
+
+        internal void DepleteEnergy(int energy)
+        {
+            this.energy -= energy;
+        }
+
     }
 }
--- ConsoleMonGame/ConsoleMonGame/Skill.cs
+++ ConsoleMonGame/ConsoleMonGame/Skill.cs
@@ -11,5 +11,11 @@ namespace ConsoleMonGame
         internal int damage;
         internal int energyCost;
         internal int name;
+
+        internal void UseOn(ConsoleMon target, ConsoleMon caster) 
+        {
+            caster.DepleteEnergy(this.energyCost);
+            target.TakeDamage(this.damage);
+        }
     }
 }
