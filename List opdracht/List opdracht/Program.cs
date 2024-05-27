93ff1fac48c0a9a17fa079bfe122a4cf3f23b264
MixmasterX28 <144898918+MixmasterX28@users.noreply.github.com>
2024-05-27 10:17:28 +0200

    07 done

---
+++ List opdracht/List opdracht/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace List_opdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> birds = new List<string>()
+            {
+                "uil", "kraai", "hond", "papagaai"
+            };
+
+            List<string> birds2 = new List<string>()
+            {
+                "meeuw", "duif"
+            };
+            
+            birds.AddRange(birds2);
+
+            birds.Remove("hond");
+
+            birds.Count();
+
+            for (int i = 0; i < birds.Count; i++)
+            {
+                Console.WriteLine(birds[i]);
+            };
+            
+        }
+    }
+}
