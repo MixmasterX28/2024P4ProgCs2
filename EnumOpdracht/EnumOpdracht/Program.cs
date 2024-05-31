24536b6ea31a56bad9717ded0660a58f88b99175
MixmasterX28 <144898918+MixmasterX28@users.noreply.github.com>
2024-05-31 15:15:31 +0200

    enumDone

---
+++ EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using System.Net.Mime;
+
+namespace EnumOpdracht
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Colors myMainThemeColor = Colors.Purple;
+
+            Console.WriteLine("Hello, World!");
+
+            if (myMainThemeColor == Colors.Purple) 
+            {
+                Console.WriteLine("dit is mijn kleur!");
+            }
+         
+
+        }
+    }
+}
