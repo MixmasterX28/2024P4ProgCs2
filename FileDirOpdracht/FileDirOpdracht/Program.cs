64d44aa55a376b7a6b53b06c71c400fe75f0d065
MixmasterX28 <144898918+MixmasterX28@users.noreply.github.com>
2024-06-21 14:45:57 +0200

    09 af

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace FileDirOpdracht
+{
+    internal class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Jamal Alniel\Desktop\Github Jamal\M4\2024P4ProgCs2");
+            foreach (FileInfo file in dir.GetFiles()) 
+            {
+                Console.WriteLine(file.Name);
+            }
+
+            foreach (DirectoryInfo directories in dir.GetDirectories()) 
+            { 
+                Console.WriteLine(directories.Name); 
+            }
+
+
+        }
+
+
+    }
+}
